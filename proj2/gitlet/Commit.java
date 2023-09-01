c51054a325661299a02aedca540a003a1170a3ba
monster898 <501018786@qq.com>
2023-09-01 12:41:38 +0800

    WIP

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -1,9 +1,9 @@
 package gitlet;
 
-// TODO: any imports you need here
-
-import java.util.Calendar;
-import java.util.Date; // TODO: You'll likely use this in this class
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -12,22 +12,18 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *  @author Hao Chen
  */
 public class Commit {
-    /**
-     * TODO: add instance variables here.
-     *
-     * List all instance variables of the Commit class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided one example for `message`.
-     */
-
     /** The timestamp of this commit was made */
-    private String timestamp = Calendar.getInstance().set(1970, 1,1);
+    private String timestamp = getCurrentTimestamp();
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss yyyy Z");
 
     /** The message of this Commit. */
     private final String message;
 
+    /** The parent commit of this commit */
     private final String parent1;
 
+    /** Another parent commit of this commit, because merge commit have two parents */
     private final String parent2;
 
     /* TODO: fill in the rest of this class. */
@@ -41,5 +37,21 @@ public class Commit {
         this.message = message;
         this.parent1 = parent1;
         this.parent2 = parent2;
+    }
+
+    public void setInitCommitTimestamp() {
+        Instant epoch = Instant.ofEpochSecond(0);
+        timestamp = epoch.atOffset(ZoneOffset.UTC).format(formatter);
+    }
+
+    private String getCurrentTimestamp() {
+        LocalDateTime dateTime = LocalDateTime.now(ZoneOffset.ofHours(8)); // Assuming +0800 as the time zone offset
+        return dateTime.format(formatter);
+    }
+
+    @Override
+    public String toString() {
+        String date = "Date: " + timestamp;
+        return date + "\n" + message;
     }
 }
